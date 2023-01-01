6a8a66f07d4dfe1dc449a5b3fbd0e79ab1c1ea62
Mikolaj Gasior <mikolaj@gasior.dev>
2023-01-01 01:02:50 +0100

    WIP Refactored stuff to extend HTMLElements

--- edytor-grid.js
+++ edytor-grid.js
@@ -1,49 +1,49 @@
-class Grid {
-    #colors = { bg: "#282828", ln1: "#555555", ln2: "#444444", ln3: "#333333" }
-    #id     = { container: "", grid: "" }
-    #ref    = { container: null, grid: null }
-
-    constructor(idContainer, idGrid) {
-        this.#id.container = idContainer;
-        this.#id.grid = idGrid;
-
-        this.#ref.container = document.getElementById(this.#id.container);
-        if (this.#ref.container == null) {
-            alert("div with id='"+this.#id.container+"' not found");
-            return;
-        }
+class EdytorGrid extends HTMLCanvasElement {
+    #colors = {
+        bg: "#282828",
+        ln1: "#555555",
+        ln2: "#444444",
+        ln3: "#333333"
     }
 
-    Init(zIndex) {
-        this.#ref.grid = document.createElement('canvas');
-        this.#ref.grid.id = this.#id.grid;
-        this.#ref.grid.classList.add('layer');
-        this.#ref.grid.style.margin = 0;
-        this.#ref.grid.style.padding = 0;
-        this.#ref.grid.style.position = "absolute";
-        this.#ref.grid.style.top = 0;
-        this.#ref.grid.style.left = 0;
-        this.#ref.grid.style.zIndex = zIndex;
-        this.#ref.container.appendChild(this.#ref.grid);
-        this.#resizeToWindow();
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.id = "grid_layer";
+        this.zIndex = 101;
+        this.classList.add('layer');
+        this.style.margin = 0;
+        this.style.padding = 0;
+        this.style.position = "absolute";
+        this.style.top = 0;
+        this.style.left = 0;
+
+        var self = this;
+        window.addEventListener("resize", function () {
+            self.#setSize();
+            self.#draw();
+        });
+
+        this.#setSize();
         this.#draw();
     }
 
-    #resizeToWindow() {
-        var w = window.innerWidth, h = window.innerHeight;
-        this.#ref.grid.width = w*2;
-        this.#ref.grid.height = h*2;
+    #setSize() {
+        this.width = window.innerWidth * 2;
+        this.height = window.innerHeight * 2;
     }
 
     #draw() {
-        var ctx = this.#ref.grid.getContext("2d");
+        var ctx = this.getContext("2d");
         ctx.fillStyle = this.#colors.gridBackground;
-        ctx.fillRect(0, 0, this.#ref.grid.width, this.#ref.grid.height);
+        ctx.fillRect(0, 0, this.width, this.height);
         ctx.lineWidth = 1;
         ctx.strokeStyle = this.#colors.ln2;
 
-        for (var i=0; i<=this.#ref.grid.width/10; i++) {
-            var j = i*10;
+        for (var i = 0; i <= this.width / 10; i++) {
+            var j = i * 10;
             ctx.beginPath();
             if (j % 100 == 0) {
                 ctx.strokeStyle = this.#colors.ln1;
@@ -53,12 +53,12 @@ class Grid {
                 ctx.strokeStyle = this.#colors.ln3;
             }
             ctx.moveTo(j, 0);
-            ctx.lineTo(j, this.#ref.grid.height);
+            ctx.lineTo(j, this.height);
             ctx.stroke();
             ctx.closePath();
         }
-        for (var i=0; i<=this.#ref.grid.height/10; i++) {
-            var j = i*10;
+        for (var i = 0; i <= this.height / 10; i++) {
+            var j = i * 10;
             ctx.beginPath();
             if (j % 100 == 0) {
                 ctx.strokeStyle = this.#colors.ln1;
@@ -68,9 +68,11 @@ class Grid {
                 ctx.strokeStyle = this.#colors.ln3;
             }
             ctx.moveTo(0, j);
-            ctx.lineTo(this.#ref.grid.width, j);
+            ctx.lineTo(this.width, j);
             ctx.stroke();
             ctx.closePath();
         }
     }
 }
+
+window.customElements.define("edytor-grid", EdytorGrid, { extends: 'canvas' });
