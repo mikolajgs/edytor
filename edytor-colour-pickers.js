8c2d54a43711de97d00870850cb1fff6d8c18d4c
Mikolaj Gasior <mikolaj@gasior.dev>
2023-03-24 10:04:16 +0100

    Add button for swapping colours

--- edytor-colour-pickers.js
+++ edytor-colour-pickers.js
@@ -22,6 +22,17 @@ class EdytorColourPickers extends HTMLElement {
 
       document.getElementById('edytor').__selectColour(t.name, i.value);
     });
+    var btn = document.createElement('button');
+    btn.innerHTML = 'Swap';
+    btn.addEventListener('click', (e) => {
+      var bgColor = document.getElementById('colour_picker_bg').value;
+      var fgColor = document.getElementById('colour_picker_fg').value;
+      document.getElementById('colour_picker_fg').value = bgColor;
+      document.getElementById('edytor').__selectColour("fg", bgColor);
+      document.getElementById('colour_picker_bg').value = fgColor;
+      document.getElementById('edytor').__selectColour("bg", fgColor);
+    });
+    this.appendChild(btn);
   }
 }
 
