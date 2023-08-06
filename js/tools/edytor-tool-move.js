b4f8fbab1884d50fd7429233e6a5ac70997c0bdf
Mikolaj Gasior <mg@forthcoming.systems>
2023-08-07 01:43:58 +0200

    Implement moving without selection

--- js/tools/edytor-tool-move.js
+++ js/tools/edytor-tool-move.js
@@ -6,6 +6,9 @@ class EdytorMoveTool extends EdytorTool {
     }
 
 
+    #startPos = [];
+
+
     constructor() {
         super();
     }
@@ -25,19 +28,60 @@ class EdytorMoveTool extends EdytorTool {
 
 
     startedCallback(x, y) {
-        return false;
+        var layer = super.getLayer(true);
+        if (layer === null) {
+            return;
+        }
+
+        this.#startPos = [x, y];
     }
 
     movedCallback(x, y, shiftKey, altKey) {
-        return false;
+        var layer = super.getLayer(true);
+        if (layer === null) {
+            return;
+        }
+        
+        layer.style.left = (x - this.#startPos[0]) + "px";
+        layer.style.top  = (y - this.#startPos[1]) + "px";
     }
 
     endedCallback(x, y, shiftKey, altKey) {
-        return false;
+        var layer = super.getLayer(true);
+        if (layer === null) {
+            return;
+        }
+        var ctx = layer.getContext("2d");
+
+        var moveX = x - this.#startPos[0];
+        var moveY = y - this.#startPos[1];
+
+        var imgData = ctx.getImageData(
+            (moveX < 0 ? Math.abs(moveX) : 0),
+            (moveY < 0 ? Math.abs(moveY) : 0),
+            layer.width - Math.abs(moveX),
+            layer.height - Math.abs(moveY)
+        );
+
+        ctx.clearRect(0, 0, layer.width, layer.height);
+        ctx.putImageData(
+            imgData,
+            (moveX < 0 ? 0 : moveX),
+            (moveY < 0 ? 0 : moveY)
+        );
+
+        layer.style.left = "0px";
+        layer.style.top  = "0px";
     }
 
     cancelledCallback() {
-        return false;
+        var layer = super.getLayer(true);
+        if (layer === null) {
+            return;
+        }
+        
+        layer.style.left = "0px";
+        layer.style.top  = "0px";
     }
 
     selectedCallback() {
